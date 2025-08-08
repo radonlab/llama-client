b9d4d9fa5e5a99bd1038849e2edcf0a3b86d21cd
skylerlee <skyler.ac.lee@gmail.com>
2025-08-08 22:46:36 +0800

    fix: fix type issue of sip

--- deps/QCefView/generate.sh
+++ deps/QCefView/generate.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-sed -e 's/QCefBrowserId/int/g' -e 's/QCefFrameId/QString/g' tpl/QCefView.sip.in
+out=$(sed -e 's/QCefBrowserId/int/g' -e 's/QCefFrameId/QString/g' tpl/QCefView.sip.in)
+sed -i '' -e '/^class/,/^};$/ {
+  r /dev/stdin
+  d
+}' sip/QCefView.sip <<< "$out"
