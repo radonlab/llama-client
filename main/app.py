43200d662891390b4c9a112e62d72fc9cbb34412
skylerlee <skyler.ac.lee@gmail.com>
2025-08-10 22:45:49 +0800

    feat: first working example

--- main/app.py
+++ main/app.py
@@ -1,11 +1,14 @@
 import sys
 
 from PyQt6.QtWidgets import QApplication
+from QCefView import QCefConfig, QCefContext
 
 
 class App(QApplication):
     def __init__(self):
         super().__init__(sys.argv)
+        self.cef_config = QCefConfig()
+        self.cef_context = QCefContext(self, sys.argv, self.cef_config)
 
     def exec(self):
         super().exec()
--- main/main_window.py
+++ main/main_window.py
@@ -1,12 +1,11 @@
 from PyQt6.QtWidgets import QMainWindow, QVBoxLayout
-from QCefView import QCefView
+from QCefView import QCefSetting, QCefView
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Llama")
-        layout = QVBoxLayout()
-        cef_view = QCefView()
-        layout.addWidget(cef_view)
-        self.setLayout(layout)
+        cef_setting = QCefSetting()
+        cef_view = QCefView("https://www.baidu.com", cef_setting)
+        self.setCentralWidget(cef_view)
